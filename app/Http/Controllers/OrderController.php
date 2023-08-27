1187879af16ce9dd3f31a72cf23e6b760b994e31
Alexis <fraumalex@gmail.com>
2023-08-26 23:59:44 -0400

    add user seeder

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -9,5 +10,16 @@ class OrderController extends Controller
     public function index(Request $request)
     {
         // Handle the payment request
+        $order = new Order();
+
+        $donation = $order->calculateDonation($request->cash);
+
+
+        $order->amount = 
+        $order->save();
+
+        return response()->json([
+            "message" => "Thanks for donation enjoy!!!"
+        ]);
     }
 }
