15078c5f41bf76146345fc0c5a3320a22d0c9294
Jorge Thomas - Akrista <23145794+Akrista@users.noreply.github.com>
2023-08-27 01:01:28 -0400

    chore(api.php): comment out the '/pay' route to disable it temporarily The '/pay' route has been commented out to disable it temporarily. This is done to prevent any unintended behavior or issues while the route is being worked on or debugged.

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/pay', [OrderController::class, 'index']);
+// Route::post('/pay', [OrderController::class, 'index']);
 Route::get('/organizations', [OrganizationsController::class, 'index']);
 Route::post('/auth/register', [AuthController::class, 'createUser']);
 Route::post('/auth/login', [AuthController::class, 'loginUser']);
