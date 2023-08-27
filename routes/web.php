b7eff82546d81e09d6bc0001cc5b286ae96f4f70
Jorge Thomas - Akrista <23145794+Akrista@users.noreply.github.com>
2023-08-26 23:51:27 -0400

    chore(web.php): remove unused route and add route for organizations to use OrganizationController@index The unused route for '/organizations' has been removed and replaced with a new route that points to the 'index' method of the 'OrganizationController' class. This change improves the code organization and ensures that the correct controller is used for handling the '/organizations' route.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrganizationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/organizations', function () {
-    return view('welcome');
-});
+Route::get('/organizations', [OrganizationController::class, 'index']);
